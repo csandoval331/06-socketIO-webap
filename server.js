9cbf61a955db26fc3e62e1952cfd36a8c15e4c94
csandoval331 <csandoval331@gmail.com>
2024-05-16 00:56:07 -0500

    adding code and notes

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const io = require('socket.io')(3000, {
         origin: '*'
     }
 })
-
+const users = {}
 
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello world')
+    // console.log('new User')
+    // socket.emit('chat-message', 'Hello world')
+
+    socket.on('new-user', name => {
+        console.log(socket)
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+        // console.log(message)
+        //socket.broadcast.emit will send message to everyone except to self
+        socket.broadcast.emit('chat-message', {message : message, name: users[socket.id]})
+    })
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+    
 })
